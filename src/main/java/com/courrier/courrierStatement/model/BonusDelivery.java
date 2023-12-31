7beaafb63248155f12d50c8da972e310fd20591e
82506892168 <perycles.vieira@serpro.gov.br>
2023-12-30 21:44:25 -0600

    feat: Models classes.

--- src/main/java/com/courrier/courrierStatement/model/BonusDelivery.java
+++ src/main/java/com/courrier/courrierStatement/model/BonusDelivery.java
@@ -1,4 +1,19 @@
 package com.courrier.courrierStatement.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BonusDelivery {
+        private UUID bonusId;
+        private UUID deliveryId;
+        private UUID courierId;
+        private Instant createdTimestamp;
+        private String value;
 }
